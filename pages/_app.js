a228533793a0266ad705a0293f08b06920fce24c
anonymous <anon@example.com>
2024-06-22 11:08:49 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,5 @@
 import '../styles/globals.css'
-import Footer from '../components/footer'
-import Header from '../components/header'
-
 export default function App({Component,props}) {
   return (<Component {...props}/>)
-  // return (
-  //   <div className='bg-black text-white'>
-  //     <Header />
-  //       <div className='min-h-screen'><Component {...props}/></div>
-  //     <Footer />
-  //   </div>
-  // );
+
 }
